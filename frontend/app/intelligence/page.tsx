d615052fcabb16aeeb08423eea207ad4fa449c9d
Ravi Prakash G R <raviprakash2662006@gmail.com>
2026-08-26 18:32:24 +0530

    feat: add intelligence route

---
+++ frontend/app/intelligence/page.tsx
@@ -0,0 +1,5 @@
+import RealDatasetIntelligence from "./RealDatasetIntelligence";
+
+export default function IntelligencePage() {
+  return <main style={{ maxWidth: 1200, margin: "0 auto", padding: "40px 24px" }}><RealDatasetIntelligence /></main>;
+}
